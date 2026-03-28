955e16801fdbc75dd0f5bc1d2f61b92716bc1779
KlarDak <mk_daking@mail.ru>
2026-03-28 22:49:41 +0300

    dev-2.3.1
    - Добавлен метод изменения директории с файлами локализаций setMisplacedDir()
    - Добавлена информация об изменениях в docs/Langs.md-файл.
    - Дополнен шаблон файла-локализации.

--- langs/Langs.php
+++ langs/Langs.php
@@ -44,6 +44,15 @@ class Langs {
         }
     }
 
+    public static function setMisplacedDir(string $dir) : bool {
+        if (is_dir($dir)) {
+            self::$path = $dir;
+            return true;
+        }
+
+        return false;
+    }
+
     /**
      * Get expression by its key
      * 
